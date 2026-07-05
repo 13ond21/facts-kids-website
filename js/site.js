8ccd38ab2c5d2a2b3a536f72f308daf9e45bc892
13ond21 <coreybond21@gmail.com>
2026-07-05 16:35:02 +0100

    Landing page with screenshots (Daily Affirmation style)

---
+++ js/site.js
@@ -0,0 +1,9 @@
+﻿(function () {
+  var header = document.getElementById("header");
+  if (!header) return;
+  function onScroll() {
+    header.classList.toggle("is-scrolled", window.scrollY > 8);
+  }
+  window.addEventListener("scroll", onScroll, { passive: true });
+  onScroll();
+})();
